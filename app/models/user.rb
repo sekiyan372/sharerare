80083590630e1c8279e09845f7236f367f9c6573
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2019-12-19 20:29:03 +0900

    modeled users

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
+
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 8 }
+end
