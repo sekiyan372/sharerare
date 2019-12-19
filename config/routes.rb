c9dddab3d1f6856689c4aaa971dcd94a5dae758b
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2019-12-19 17:34:41 +0900

    layout maked

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/sign_up'
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
+  get '/signup', to: 'users#signup'
 end
